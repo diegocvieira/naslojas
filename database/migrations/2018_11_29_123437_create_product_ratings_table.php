59a03cb5cd485b379245bf39075f9de737e3525f
Diego Vieira <diegovc10@hotmail.com>
2018-12-02 09:49:16 -0200

    migration fixed

--- database/migrations/2018_11_29_123437_create_product_ratings_table.php
+++ database/migrations/2018_11_29_123437_create_product_ratings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRatingsTable extends Migration
+class CreateProductRatingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateRatingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('ratings', function (Blueprint $table) {
+        Schema::create('product_ratings', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
@@ -31,6 +31,6 @@ class CreateRatingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ratings');
+        Schema::dropIfExists('product_ratings');
     }
 }
