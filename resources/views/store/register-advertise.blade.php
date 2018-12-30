363c6847bb8d95e74d68934076cce1b2c8e97fe3
Diego Vieira <diegovc10@hotmail.com>
2018-12-30 20:24:49 -0200

    Edit advertise

--- resources/views/store/register-advertise.blade.php
+++ resources/views/store/register-advertise.blade.php
@@ -18,40 +18,40 @@
 
         <div class="cards">
             <div class="card">
-                <p>Atraia mais clientes <br> para dentro da loja</p>
+                <p>Atraia mais clientes<br>para dentro da loja</p>
             </div>
 
             <div class="card">
-                <p>Suas ofertas disponíveis <br> 24hs por dia</p>
+                <p>Suas ofertas disponíveis<br>24hs por dia</p>
             </div>
 
             <div class="card">
-                <p>Tenha uma vitrine <br> virtual da sua loja</p>
+                <p>Tenha uma vitrine<br>virtual da sua loja</p>
             </div>
 
             <div class="card">
-                <p>Suas ofertas <br> no site e no app</p>
+                <p>Suas ofertas<br>no site e no app</p>
             </div>
 
             <div class="card">
-                <p>Tecnologia e inovação <br> para a sua loja</p>
+                <p>Tecnologia e inovação<br>para a sua loja</p>
             </div>
         </div>
 
         <div class="steps">
             <div class="step">
                 <img src="{{ asset('images/advise/shirt.png') }}" class="img-shirt" />
-                <p>Nós buscamos os <br> produtos na sua loja</p>
+                <p>Nós buscamos os<br>produtos na sua loja</p>
             </div>
 
             <div class="step">
                 <img src="{{ asset('images/advise/camera.png') }}" class="img-camera" />
-                <p>Tiramos fotos lindas e <br> cadastramos no site</p>
+                <p>Tiramos fotos lindas e<br>cadastramos no site</p>
             </div>
 
             <div class="step">
                 <img src="{{ asset('images/advise/truck.png') }}" class="img-truck" />
-                <p>Devolvemos para <br> você em 1 dia útil</p>
+                <p>Devolvemos para<br>você em 1 dia útil</p>
             </div>
         </div>
 
@@ -64,29 +64,29 @@
         </div>
 
         <div class="price">
-            <p>SEM MENSALIDADE</p>
+            <p>SEM MENSALIDADE<br>Até 30 produtos por mês é</p>
 
-            <h3>Apenas <span><span>R$</span>0,30</span> por produto</h3>
+            <h3>TOTALMENTE GRÁTIS</h3>
 
-            <p class="taxe">+ taxa única de R$ 5,00 de transporte</p>
+            <p class="taxe">Após pague apenas R$ 0,50 por oferta + taxa única de R$ 5,00 do transporte</p>
         </div>
 
         <div class="race">
             <span class="vertical-line"></span>
 
-            <p>O que está esperando? <br> Saia na frente da concorrência este ano</p>
+            <p>O que está esperando?<br>Saia na frente da concorrência este ano</p>
 
             <img src="{{ asset('images/advise/race.png') }}" />
         </div>
 
         <div class="test">
-            <p>Faça um teste de compromisso <br> As primeiras <b>50 ofertas</b> são</p>
+            <p>Faça um teste sem compromisso<br>Até <b>30 ofertas</b> por mês é</p>
 
             <h3>TOTALMENTE GRÁTIS</h3>
 
             <a href="{{ route('store-register-get') }}" class="btn-advertise">DIVULGAR OFERTAS</a>
 
-            <p class="phone">Dúvidas no whatsApp da empresa <br> 53 9 9169 1716</p>
+            <p class="phone">Dúvidas no whatsApp da empresa<br>53 9 9169 1716</p>
         </div>
     </div>
 @endsection
