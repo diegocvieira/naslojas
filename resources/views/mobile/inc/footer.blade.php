3132d63b034a32d8298bd64034306c0544f755f4
Diego Vieira <diegovc10@hotmail.com>
2019-01-20 22:35:25 -0200

    Tawk removed

--- resources/views/mobile/inc/footer.blade.php
+++ resources/views/mobile/inc/footer.blade.php
@@ -77,20 +77,6 @@
             <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.8/jquery.mask.min.js"></script>
         @endif
 
-        <!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/59810f554471ce54db6521a4/default';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-        </script>
-        <!--End of Tawk.to Script-->
-
         <!-- Hotjar Tracking Code for www.naslojas.com -->
         <script>
             (function(h,o,t,j,a,r){
