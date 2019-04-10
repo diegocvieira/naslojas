f99929a46772d04818d7b1d8a32b96e81e6e5e5c
Diego Vieira <diegovc10@hotmail.com>
2019-04-10 12:25:45 -0300

    hide icon

--- resources/views/store/list-product-edit.blade.php
+++ resources/views/store/list-product-edit.blade.php
@@ -44,7 +44,7 @@
                 <button type="button" class="select-color color-variation {{ !$product->related ? 'hidden' : '' }}" data-url="{{ route('color-variation') }}" title="Remover variação de cor"></button>
                 <button type="button" class="select-color {{ $product->related ? 'hidden' : '' }}" title="Selecionar variação"></button>
 
-                <button type="button" class="free-freight {{ $product->free_freight ? 'free-freight-selected' : '' }}" title="{{ $product->free_freight ? 'Desabilitar frete grátis' : 'Habilitar frete grátis' }}" data-url="{{ route('product-free-freight') }}">frete grátis</button>
+                <?php /* <button type="button" class="free-freight {{ $product->free_freight ? 'free-freight-selected' : '' }}" title="{{ $product->free_freight ? 'Desabilitar frete grátis' : 'Habilitar frete grátis' }}" data-url="{{ route('product-free-freight') }}">frete grátis</button> */ ?>
             </div>
         </div>
 
