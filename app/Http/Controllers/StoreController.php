45022020db6969e3b9771c877776c09b030d53c1
Diego Vieira <diegovc10@hotmail.com>
2019-04-24 22:22:21 -0300

    Remove option add/edit product from menu and change login redirect

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -167,7 +167,7 @@ class StoreController extends Controller
     {
         if (Auth::guard('store')->attempt(['email' => $request->email, 'password' => $request->password], true)) {
             $return['status'] = true;
-            $return['url'] = Auth::guard('store')->user()->store->status ? route('show-store', Auth::guard('store')->user()->store->slug) : route('edit-products');
+            $return['url'] = Auth::guard('store')->user()->store->status ? route('show-store', Auth::guard('store')->user()->store->slug) : route('get-store-config');
         } else {
             $return['status'] = false;
             $return['msg'] = 'Não identificamos o e-mail e/ou a senha que você informou.';
