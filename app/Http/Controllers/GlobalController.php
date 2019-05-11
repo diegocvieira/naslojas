54401de25449b02663ddd402cf21a09966baca36
Diego Vieira <diegovc10@hotmail.com>
2019-05-11 01:13:37 -0300

    Fix new home

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -20,11 +20,69 @@ class GlobalController extends Controller
 
         $products = Product::has('images')->inRandomOrder()->paginate(30);
 
-        $featured_products = Product::has('images')->inRandomOrder()->limit(15)->get();
+        $featured_products = Product::has('images')
+            ->where(function($q) {
+                $q->where('identifier', '1369251634')
+                    ->orWhere('identifier', '7682726781')
+                    ->orWhere('identifier', '6284040866')
+                    ->orWhere('identifier', '1240659416')
+                    ->orWhere('identifier', '6813927890')
+                    ->orWhere('identifier', '2824134342')
+                    ->orWhere('identifier', '4360979453')
+                    ->orWhere('identifier', '8735695318')
+                    ->orWhere('identifier', '7398215376')
+                    ->orWhere('identifier', '3118471542')
+                    ->orWhere('identifier', '3428785629')
+                    ->orWhere('identifier', '9462447778')
+                    ->orWhere('identifier', '6258734190')
+                    ->orWhere('identifier', '8431415355')
+                    ->orWhere('identifier', '6570416578')
+                    ->orWhere('identifier', '8563896576')
+                    ->orWhere('identifier', '4998815173')
+                    ->orWhere('identifier', '1797473126')
+                    ->orWhere('identifier', '7702900754')
+                    ->orWhere('identifier', '4816368777')
+                    ->orWhere('identifier', '2908097247')
+                    ->orWhere('identifier', '7374498917')
+                    ->orWhere('identifier', '2602996432')
+                    ->orWhere('identifier', '3274134989')
+                    ->orWhere('identifier', '9500944073');
+            })
+            ->inRandomOrder()
+            ->get();
 
-        $offers = Product::has('images')->inRandomOrder()->limit(15)->get();
+        $offers = Product::has('images')
+            ->where(function($q) {
+                $q->where('identifier', '1545220228')
+                    ->orWhere('identifier', '1522012655')
+                    ->orWhere('identifier', '9527520242')
+                    ->orWhere('identifier', '3144605048')
+                    ->orWhere('identifier', '5598752579')
+                    ->orWhere('identifier', '9271930957')
+                    ->orWhere('identifier', '5183128253')
+                    ->orWhere('identifier', '8912953312')
+                    ->orWhere('identifier', '5569228901')
+                    ->orWhere('identifier', '7540756864')
+                    ->orWhere('identifier', '5550121493')
+                    ->orWhere('identifier', '7631231647')
+                    ->orWhere('identifier', '5465437311')
+                    ->orWhere('identifier', '3540796156')
+                    ->orWhere('identifier', '7283592430')
+                    ->orWhere('identifier', '8379221556')
+                    ->orWhere('identifier', '6482179871')
+                    ->orWhere('identifier', '7292324672')
+                    ->orWhere('identifier', '1688774253')
+                    ->orWhere('identifier', '5673353306')
+                    ->orWhere('identifier', '8402541905')
+                    ->orWhere('identifier', '6032595789')
+                    ->orWhere('identifier', '2998447243')
+                    ->orWhere('identifier', '8115705904')
+                    ->orWhere('identifier', '6388814493');
+            })
+            ->inRandomOrder()
+            ->get();
 
-        $trending_words = ['melissa', 'krause', 'hercilio', 'emilice', 'sapato', 'calcado', 'tenis', 'sandalia', 'bota', 'salto alto', 'scarpin', 'camisa', 'camiseta', 'vestido', 'saia', 'bermuda', 'short', 'jeans', 'casaco', 'jaqueta', 'masculino', 'feminino', 'nike', 'adidas', 'mizuno', 'schutz', 'colcci', 'celular', 'livro', 'oculos de sol', 'maquiagem'];
+        $trending_words = ['melissa', 'krause', 'hercilio', 'emilice', 'sapato', 'tenis', 'sandalia', 'bota', 'salto alto', 'scarpin', 'camisa', 'camiseta', 'vestido', 'saia', 'short', 'jeans', 'casaco', 'jaqueta', 'masculino', 'feminino', 'nike', 'adidas', 'schutz', 'colcci', 'livro', 'oculos de sol', 'maquiagem'];
         shuffle($trending_words);
 
         $brands = ['adidas', 'asics', 'bebece', 'bull-terrier', 'colcci', 'grendene', 'melissa', 'nike', 'olympikus', 'ramarim', 'schutz', 'via-marte'];
