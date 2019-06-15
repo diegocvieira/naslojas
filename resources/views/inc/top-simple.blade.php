048d627c8a357a2deb1b3ca60244dfef20966103
Diego Vieira <diegovc10@hotmail.com>
2019-06-15 16:08:50 -0300

    Add link para sair da conta da central

--- resources/views/inc/top-simple.blade.php
+++ resources/views/inc/top-simple.blade.php
@@ -3,5 +3,9 @@
         <a href="{{ url('/') }}" id="logo-naslojas">
             <img src="{{ asset('images/logo-naslojas.png') }}" />
         </a>
+
+        @if (session('central_logged'))
+            <a href="{{ route('central-logout') }}" class="logout">Sair</a>
+        @endif
     </div>
 </header>
